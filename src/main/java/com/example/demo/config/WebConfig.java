850efa27c0e7ecb8780cf50cf3f31577a225fd71
ohsanghy09 <ohsanghy09@naver.com>
2024-09-02 18:38:33 +0900

    토큰 활성화 완료

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -10,12 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:8080") // Vue.js 서버 주소
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .exposedHeaders("Authorization", "X-Auth-Token") // 이 부분이 중요합니다.
-                    .allowCredentials(true);
+            registry.addMapping("/**")  // 들어오는 모든 요청 허용
+                    .allowedOrigins("http://localhost:8081") // 특정 서버 주소 허용
+                    // 둘 중에 하나 사용
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용되는 메서드
+                    .allowedHeaders("*")    // 모든 헤더 허용
+                    .exposedHeaders("Authorization", "X-Auth-Token") // 특정 헤더 허용
+                    .allowCredentials(true);    // 자격 증명 허용
         }
     }
 
